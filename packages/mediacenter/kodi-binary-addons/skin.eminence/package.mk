01a5b94f9efbb8fd87dccbf568f56d2012296261
Drummas <matreco2008@gmail.com>
2023-04-26 11:52:33 +0100

    Update package.mk

--- packages/mediacenter/kodi-binary-addons/skin.eminence/package.mk
+++ packages/mediacenter/kodi-binary-addons/skin.eminence/package.mk
@@ -1,1 +1,19 @@
-teste
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="skin.eminence"
+PKG_VERSION="v4.1.20b"
+PKG_SHA256="dc9f90607de0b8d8a5e72a7016a6096fa944efa4e58c6ff135088fa6a6ff2b9c"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/jurialmunkey/skin.eminence.2"
+PKG_URL="https://github.com/jurialmunkey/skin.eminence.2/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform pugixml zlib xz"
+PKG_SECTION=""
+PKG_SHORTDESC="skin.eminence"
+PKG_LONGDESC="skin.eminence"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.skin"
