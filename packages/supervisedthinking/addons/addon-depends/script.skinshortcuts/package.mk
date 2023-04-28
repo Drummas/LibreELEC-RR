d5a4873c24a078e57ca40dc8bbc2e8e8111742db
Drummas <matreco2008@gmail.com>
2023-04-28 22:08:12 +0100

    Update package.mk

--- packages/supervisedthinking/addons/addon-depends/script.skinshortcuts/package.mk
+++ packages/supervisedthinking/addons/addon-depends/script.skinshortcuts/package.mk
@@ -1,1 +1,19 @@
-https://github.com/MikeSiLVO/script.skinshortcuts.git
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present BigNoid, marcelveldt, SiLVO, anxdpanic
+
+
+PKG_NAME="skinshortcuts"
+PKG_VERSION=""
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SHA256="38da3342e2c0d4d597fd2a2833b046c7fc98c82a454e5396d70e07bc2eb953d4"
+PKG_SITE="https://github.com/MikeSiLVO/script.skinshortcuts"
+PKG_URL="https://github.com/MikeSiLVO/script.skinshortcuts.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Skin Shortcuts"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  :
+}
