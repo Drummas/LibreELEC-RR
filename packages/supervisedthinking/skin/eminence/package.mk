69215ef37dee993bb3b9aee40b52170b379f10b4
Drummas <matreco2008@gmail.com>
2023-04-28 22:09:23 +0100

    Update package.mk

--- packages/supervisedthinking/skin/eminence/package.mk
+++ packages/supervisedthinking/skin/eminence/package.mk
@@ -4,13 +4,15 @@
 
 
 PKG_NAME="eminence"
-PKG_VERSION="v4.1.20b"
+#PKG_VERSION="v4.1.20b"
+PKG_VERSION=""
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SHA256="38da3342e2c0d4d597fd2a2833b046c7fc98c82a454e5396d70e07bc2eb953d4"
 PKG_SITE="https://github.com/jurialmunkey/skin.eminence.2"
-PKG_URL="https://github.com/jurialmunkey/skin.eminence.2/archive/${PKG_VERSION}.tar.gz"
+#PKG_URL="https://github.com/jurialmunkey/skin.eminence.2/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/jurialmunkey/skin.eminence.2.git"
 PKG_DEPENDS_TARGET="toolchain kodi-platform pugixml zlib xz"
 PKG_DEPENDS_UNPACK="kodi"
 PKG_SECTION="skin"
