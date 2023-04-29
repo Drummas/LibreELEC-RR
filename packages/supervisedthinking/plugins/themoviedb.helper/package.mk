10459be05045ed176e1885c6a6955062ef9bfa69
Drummas <matreco2008@gmail.com>
2023-04-30 00:56:44 +0100

    Create package.mk

---
+++ packages/supervisedthinking/plugins/themoviedb.helper/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+
+PKG_NAME="themoviedb"
+PKG_VERSION="v5.1.11"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SHA256="38da3342e2c0d4d597fd2a2833b046c7fc98c82a454e5396d70e07bc2eb953d4"
+PKG_SITE="https://github.com/jurialmunkey/plugin.video.themoviedb.helper"
+PKG_URL="https://github.com/jurialmunkey/plugin.video.themoviedb.helper/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="TheMovieDB Helper"
+PKG_LONGDESC="TheMovieDB Helper"
+PKG_TOOLCHAIN="manual"
