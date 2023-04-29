eb7e6ec7af2f25b26ea5c3c0286046b15f4a7824
Drummas <matreco2008@gmail.com>
2023-04-30 00:58:50 +0100

    Create package.mk

---
+++ packages/supervisedthinking/resources/weathericons-white/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+
+PKG_NAME="weathericons-white"
+PKG_VERSION="master"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SHA256="38da3342e2c0d4d597fd2a2833b046c7fc98c82a454e5396d70e07bc2eb953d4"
+PKG_SITE="https://github.com/XBMC-Addons/resource.images.weathericons.white"
+PKG_URL="https://github.com/XBMC-Addons/resource.images.weathericons.white/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="Weather Icons White"
+PKG_LONGDESC="Weather Icons White"
+PKG_TOOLCHAIN="manual"
