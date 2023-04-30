715c8bf72f6d686e2ed7a20ba4b4ed9c45a8ccb0
Drummas <matreco2008@gmail.com>
2023-04-30 01:00:43 +0100

    Create package.mk

---
+++ packages/supervisedthinking/script/image-resource/package.mk
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present BigNoid, marcelveldt, SiLVO, anxdpanic
+
+
+PKG_NAME="imageresource"
+PKG_VERSION="master"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SHA256="a0c61c6e7319f77f3ace12eda6b505649c1ac542eecb44e4bc4bc785f77d753b"
+PKG_SITE="https://github.com/phil65/script.image.resource.select"
+PKG_URL="https://github.com/phil65/script.image.resource.select/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SHORTDESC="Image Resource Select"
+PKG_LONGDESC="Image Resource Select"
+PKG_TOOLCHAIN="manual"
